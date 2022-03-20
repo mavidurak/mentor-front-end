8b4b18587eb44b42bf72e4a0ffc4e37c6ed89fee
DEONSKY <bestrasen@hotmail.com>
2022-03-20 20:44:04 +0300

    fixes

--- src/helpers/axios.js
+++ src/helpers/axios.js
@@ -19,7 +19,7 @@ axios.interceptors.response.use(function (response) {
     toast.dismiss(response.config.requestToastId)
   } else {
     toast.update(response.config.requestToastId, {
-      render: response.data.message,
+      render: response.data?.message,
       type: "success",
       isLoading: false,
       autoClose: 5000,
